86dc5eba0972e9e1ab82950c805b7c9526d277c6
Steven L. Speek <slspeek@gmail.com>
2024-04-23 14:01:41 +0200

    memsize

--- spec.sh
+++ spec.sh
@@ -4,7 +4,7 @@ DEBIAN_VERSION=$(cat /etc/debian_version)
 CPUMODEL=$(inxi --cpu -y1|grep model|cut -d: -f2-|cut -c2-)
 DISKTYPE=$(inxi -Dxxx -y1|grep type|cut -d: -f2-|cut -c2-)
 DISKSIZE=$(inxi -Dxxx -y1|grep total|cut -d: -f2-|cut -c2-)
-MEMSIZE=$(free -h|grep Mem|cut -c16-|cut -d' ' -f1|sed -e 's/Gi/ GiB/')
+MEMSIZE=$(free -h|grep Mem|cut -d: -f2-|sed -e 's/^[ ]*//'|cut -d' ' -f1-2 |sed -e 's/Gi/ GiB/')
 MEMTYPE=$(sudo inxi --memory-modules -y1 |grep type|cut -d: -f2-|cut -c2-)
 USERNAME=$(id -nu 1000)
 PROBE_CMD=${PROBE_CMD:-sudo hw-probe -all -upload}
@@ -18,7 +18,6 @@ cat > $BUILD/spec.tex <<EOF
 \newcommand{\bootmenukey}{$BOOTMENUKEY}
 \newcommand{\disksize}{$DISKSIZE}
 \newcommand{\disktype}{$DISKTYPE}
-\newcommand{\corecount}{$CORECOUNT}
 \newcommand{\memsize}{$MEMSIZE}
 \newcommand{\memtype}{$MEMTYPE}
 \newcommand{\debianversion}{$DEBIAN_VERSION}
